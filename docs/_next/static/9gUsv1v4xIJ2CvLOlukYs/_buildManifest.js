503491b95cec211c013adf3967fa54ed8a3ac941
clfhhc <david.chen@minted.com>
2020-05-23 23:20:10 -0700

    deploy

---
+++ docs/_next/static/9gUsv1v4xIJ2CvLOlukYs/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = (function(a){return {"/":[a],"/index":[a]}}("static\u002Fchunks\u002F9f96d65d.a86943d4b2eb7629e8eb.js"));self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
