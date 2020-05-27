caf42e10863279f3a1f5c9007ef6a31c346c3f74
clfhhc <david.chen@minted.com>
2020-05-27 12:25:01 -0700

    add auto next step to choice button

--- src/features/scriptDisplay/ChoiceButtons.tsx
+++ src/features/scriptDisplay/ChoiceButtons.tsx
@@ -4,28 +4,55 @@ import {
   updateSelectedChoiceId,
   GlobalChoiceControlState,
 } from 'features/choiceControl/choiceControlSlice';
-import { selectCurrentStepId } from 'features/step/selector';
+import {
+  selectCurrentStepId,
+  selectCheckScriptStep,
+} from 'features/step/selector';
 import choiceControlAdapter from 'features/choiceControl/choiceControlEntity';
 import { selectChoiceControl } from 'features/choiceControl/selector';
+import stepAdapter from 'features/step/stepEntity';
+import { getNextVisibleStepId } from 'features/step/getStepId';
+import { changeCurrentStepId } from 'features/step/stepSlice';
 import ChoiceButton from './ChoiceButton';
 
 const {
   selectById: selectChoiceControlById,
 } = choiceControlAdapter.getSelectors(selectChoiceControl);
 
+const {
+  selectIds: selectStepIds,
+  selectEntities: selectStepEntities,
+} = stepAdapter.getSelectors(selectCheckScriptStep);
+
 const ChoiceButtons: FC = () => {
   const stepId = useSelector(selectCurrentStepId) || '';
   const choiceControl = useSelector((state: GlobalChoiceControlState) =>
     selectChoiceControlById(state, stepId)
   );
+  const ids = useSelector(selectStepIds);
+  const entities = useSelector(selectStepEntities);
   const dispatch = useDispatch();
   const handleClick = useCallback(
     (choiceId: string) => {
       if (stepId) {
         dispatch(updateSelectedChoiceId({ stepId, choiceId }));
+        const nextStepId = getNextVisibleStepId({
+          ids,
+          entities,
+          startId: stepId,
+        });
+        if (nextStepId) {
+          dispatch(changeCurrentStepId(nextStepId));
+        }
       }
     },
-    [stepId, dispatch, updateSelectedChoiceId]
+    [
+      stepId,
+      dispatch,
+      updateSelectedChoiceId,
+      getNextVisibleStepId,
+      changeCurrentStepId,
+    ]
   );
   return (
     <div>
