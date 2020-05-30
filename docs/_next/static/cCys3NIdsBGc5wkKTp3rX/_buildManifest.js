4df21934fe66c8c41ed6f2a9a29546d3499e66a2
clfhhc <david.chen@minted.com>
2020-05-30 00:54:06 -0700

    deploy

---
+++ docs/_next/static/cCys3NIdsBGc5wkKTp3rX/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = (function(a,b){return {"/":[b,a],"/[aspect]":[a],"/index":[b,a]}}("static\u002Fchunks\u002F5cdaca46d7c2518dd7983442609c9508174c460e.e853a04edba3c28566ec.js","static\u002Fchunks\u002F9f96d65d.8c5ceff6ad3e13ecd923.js"));self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
--- docs/_next/static/runtime/webpack.js
+++ docs/_next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "9e20324e565ea1ea7369";
+/******/ 	var hotCurrentHash = "6dc849602ec4be2ccc88";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
