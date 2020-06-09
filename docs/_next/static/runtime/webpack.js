cfcfc557517a0c4c5b467c80a634fe518f7c9158
clfhhc <david.chen@minted.com>
2020-06-09 00:44:51 -0700

    deploy

--- docs/_next/static/runtime/webpack.js
+++ docs/_next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "fbed36ecb036701d5594";
+/******/ 	var hotCurrentHash = "d8a6495e785066b26357";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
